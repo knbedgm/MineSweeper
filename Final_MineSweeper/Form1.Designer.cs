3e0b25570151c3c4c29935d03a8f7f3512be7798
zaintennis <56123175+zaintennis@users.noreply.github.com>
2020-01-09 09:41:04 -0500

    created functions, commented, decided on ui size

--- Final_MineSweeper/Form1.Designer.cs
+++ Final_MineSweeper/Form1.Designer.cs
@@ -28,35 +28,40 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// pictureBox1
-			// 
-			this.pictureBox1.Location = new System.Drawing.Point(12, 13);
-			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(355, 317);
-			this.pictureBox1.TabIndex = 0;
-			this.pictureBox1.TabStop = false;
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(641, 478);
-			this.Controls.Add(this.pictureBox1);
-			this.Name = "Form1";
-			this.Text = "Form1";
-			this.Load += new System.EventHandler(this.Form1_Load);
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-			this.ResumeLayout(false);
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(270, 270);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 345);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Minesweeper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.PictureBox pictureBox1;
-	}
+        private System.Windows.Forms.Timer timer1;
+    }
 }
 
--- Final_MineSweeper/Form1.cs
+++ Final_MineSweeper/Form1.cs
@@ -10,8 +10,8 @@ using System.Windows.Forms;
 
 namespace Final_MineSweeper
 {
-	public partial class Form1 : Form
-	{
+    public partial class Form1 : Form
+    {
 		public delegate void tileCallback(Tile t);
 		public Form1()
 		{
@@ -19,35 +19,70 @@ namespace Final_MineSweeper
 		}
 
 		List<List<Tile>> mineField = new List<List<Tile>>();
-		int height = 9, length = 9, mineCount = 10;
+		int height = 9, width = 9, mineCount = 10;
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
 			NewGame();
 		}
 
-		public void NewGame()
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void NewGame()
 		{
+			NewMinefeild();
+			GenerateMines();
+		}
+
+        public void NewMinefeild()
+        {
 			mineField.Clear();
 			for (int i = 0; i < height; i++)
 			{
-				List<Tile> row = new List<Tile>();
-				for (int j = 0; i < length; j++)
+				List<Tile> col = new List<Tile>();
+				for (int j = 0; j < width; j++)
 				{
-					row.Add(new Tile(i, j));
+					col.Add(new Tile(i, j));
 				}
-				mineField.Add(row);
+				mineField.Add(col);
 			}
-			mineField.ForEach((List<Tile> row) => {
-				row.ForEach((Tile t) => {
+			mineField.ForEach((List<Tile> col) => {
+				col.ForEach((Tile t) => {
 					ForNeighbors(t, t.AddBomb);
 				});
 			});
 		}
 
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+			mineField.ForEach((List<Tile> col) => {
+				col.ForEach((Tile t) => {
+					t.Draw(e.Graphics);
+				});
+			});
+        }
+
+        public void GenerateMines() {
+			int bombsAdded = 0;
+			Random r = new Random();
+
+			while (bombsAdded < mineCount) {
+				int x = r.Next(0,height);
+				int y = r.Next(0,width);
+
+				if (!mineField[x][y].isBomb) {
+					mineField[x][y].isBomb = true;
+					bombsAdded++;
+				}
+			}
+		}
+
 		public void ForNeighbors(Tile t, tileCallback callback)
 		{
-
+           
 		}
 
 	}
@@ -65,7 +100,7 @@ namespace Final_MineSweeper
 		public bool isBomb = false;
 		public byte bombsNear = 0;
 
-		public int size = 15;
+		public int size = 30;
 		public int x, y;
 
 		public Tile(int x, int y)
@@ -84,7 +119,14 @@ namespace Final_MineSweeper
 
 		public void Draw(Graphics g)
 		{
-			// draw tile
-		}
+			
+			SolidBrush b = new SolidBrush(Color.LightGray);
+			Pen p = new Pen(Color.Black, 1);
+            g.FillRectangle(b, x*size, y*size, size, size);
+			g.DrawLine(p, x*size+size-1, y*size, x*size+size-1, y*size+size);
+			g.DrawLine(p, x*size, y*size+size-1, x*size+size, y*size+size-1);
+          
+        }
 	}
+   
 }
