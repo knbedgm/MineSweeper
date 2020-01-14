3f57e206e301e7fe45493305b02d51cfa7d8b2cd
Kyle Benton <kyle@paulandnat.com>
2020-01-14 13:37:44 -0500

    added recurseave opening, numbers, updated timing system

--- Final_MineSweeper/Form1.Designer.cs
+++ Final_MineSweeper/Form1.Designer.cs
@@ -28,148 +28,150 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.canvas = new System.Windows.Forms.PictureBox();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.btnStart = new System.Windows.Forms.Button();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.easyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.mediumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.hardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.lblTimer = new System.Windows.Forms.Label();
-            this.lblFlags = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
-            this.menuStrip1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // canvas
-            // 
-            this.canvas.Location = new System.Drawing.Point(154, 162);
-            this.canvas.Name = "canvas";
-            this.canvas.Size = new System.Drawing.Size(270, 270);
-            this.canvas.TabIndex = 0;
-            this.canvas.TabStop = false;
-            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.Canvas_Paint);
-            this.canvas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseClick);
-            // 
-            // timer1
-            // 
-            this.timer1.Interval = 1000;
-            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
-            // 
-            // btnStart
-            // 
-            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnStart.Location = new System.Drawing.Point(154, 435);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(270, 34);
-            this.btnStart.TabIndex = 1;
-            this.btnStart.Text = "Start Game";
-            this.btnStart.UseVisualStyleBackColor = true;
-            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.canvas = new System.Windows.Forms.PictureBox();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.btnStart = new System.Windows.Forms.Button();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.easyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.mediumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.hardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.lblTimer = new System.Windows.Forms.Label();
+			this.lblFlags = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// canvas
+			// 
+			this.canvas.Location = new System.Drawing.Point(154, 162);
+			this.canvas.Name = "canvas";
+			this.canvas.Size = new System.Drawing.Size(270, 270);
+			this.canvas.TabIndex = 0;
+			this.canvas.TabStop = false;
+			this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.Canvas_Paint);
+			this.canvas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseClick);
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+			// 
+			// btnStart
+			// 
+			this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnStart.Location = new System.Drawing.Point(154, 435);
+			this.btnStart.Name = "btnStart";
+			this.btnStart.Size = new System.Drawing.Size(270, 34);
+			this.btnStart.TabIndex = 1;
+			this.btnStart.Text = "New Game";
+			this.btnStart.UseVisualStyleBackColor = true;
+			this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.gameToolStripMenuItem,
             this.helpToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(596, 24);
-            this.menuStrip1.TabIndex = 4;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // gameToolStripMenuItem
-            // 
-            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(596, 24);
+			this.menuStrip1.TabIndex = 4;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// gameToolStripMenuItem
+			// 
+			this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.aboutToolStripMenuItem,
             this.easyToolStripMenuItem,
             this.mediumToolStripMenuItem,
             this.hardToolStripMenuItem});
-            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
-            this.gameToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
-            this.gameToolStripMenuItem.Text = "Game";
-            // 
-            // aboutToolStripMenuItem
-            // 
-            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
-            this.aboutToolStripMenuItem.Text = "About";
-            // 
-            // easyToolStripMenuItem
-            // 
-            this.easyToolStripMenuItem.Name = "easyToolStripMenuItem";
-            this.easyToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
-            this.easyToolStripMenuItem.Text = "Easy ";
-            // 
-            // mediumToolStripMenuItem
-            // 
-            this.mediumToolStripMenuItem.Name = "mediumToolStripMenuItem";
-            this.mediumToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
-            this.mediumToolStripMenuItem.Text = "Medium ";
-            // 
-            // hardToolStripMenuItem
-            // 
-            this.hardToolStripMenuItem.Name = "hardToolStripMenuItem";
-            this.hardToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
-            this.hardToolStripMenuItem.Text = "Hard";
-            // 
-            // helpToolStripMenuItem
-            // 
-            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
-            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
-            this.helpToolStripMenuItem.Text = "Help";
-            // 
-            // lblTimer
-            // 
-            this.lblTimer.AutoSize = true;
-            this.lblTimer.BackColor = System.Drawing.Color.Transparent;
-            this.lblTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTimer.ForeColor = System.Drawing.Color.Red;
-            this.lblTimer.Location = new System.Drawing.Point(403, 130);
-            this.lblTimer.Name = "lblTimer";
-            this.lblTimer.Size = new System.Drawing.Size(0, 25);
-            this.lblTimer.TabIndex = 5;
-            // 
-            // lblFlags
-            // 
-            this.lblFlags.AutoSize = true;
-            this.lblFlags.BackColor = System.Drawing.Color.Transparent;
-            this.lblFlags.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFlags.ForeColor = System.Drawing.Color.Red;
-            this.lblFlags.Location = new System.Drawing.Point(156, 130);
-            this.lblFlags.Name = "lblFlags";
-            this.lblFlags.Size = new System.Drawing.Size(0, 25);
-            this.lblFlags.TabIndex = 6;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(596, 526);
-            this.Controls.Add(this.lblFlags);
-            this.Controls.Add(this.lblTimer);
-            this.Controls.Add(this.btnStart);
-            this.Controls.Add(this.canvas);
-            this.Controls.Add(this.menuStrip1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MainMenuStrip = this.menuStrip1;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "Form1";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Minesweeper";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+			this.gameToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+			this.gameToolStripMenuItem.Text = "Game";
+			// 
+			// aboutToolStripMenuItem
+			// 
+			this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+			this.aboutToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+			this.aboutToolStripMenuItem.Text = "About";
+			// 
+			// easyToolStripMenuItem
+			// 
+			this.easyToolStripMenuItem.Name = "easyToolStripMenuItem";
+			this.easyToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+			this.easyToolStripMenuItem.Text = "Easy ";
+			// 
+			// mediumToolStripMenuItem
+			// 
+			this.mediumToolStripMenuItem.Name = "mediumToolStripMenuItem";
+			this.mediumToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+			this.mediumToolStripMenuItem.Text = "Medium ";
+			// 
+			// hardToolStripMenuItem
+			// 
+			this.hardToolStripMenuItem.Name = "hardToolStripMenuItem";
+			this.hardToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+			this.hardToolStripMenuItem.Text = "Hard";
+			// 
+			// helpToolStripMenuItem
+			// 
+			this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+			this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+			this.helpToolStripMenuItem.Text = "Help";
+			// 
+			// lblTimer
+			// 
+			this.lblTimer.AutoSize = true;
+			this.lblTimer.BackColor = System.Drawing.Color.Transparent;
+			this.lblTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTimer.ForeColor = System.Drawing.Color.Red;
+			this.lblTimer.Location = new System.Drawing.Point(393, 130);
+			this.lblTimer.Name = "lblTimer";
+			this.lblTimer.Size = new System.Drawing.Size(38, 25);
+			this.lblTimer.TabIndex = 5;
+			this.lblTimer.Text = "99";
+			this.lblTimer.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			// 
+			// lblFlags
+			// 
+			this.lblFlags.AutoSize = true;
+			this.lblFlags.BackColor = System.Drawing.Color.Transparent;
+			this.lblFlags.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblFlags.ForeColor = System.Drawing.Color.Red;
+			this.lblFlags.Location = new System.Drawing.Point(149, 130);
+			this.lblFlags.Name = "lblFlags";
+			this.lblFlags.Size = new System.Drawing.Size(38, 25);
+			this.lblFlags.TabIndex = 6;
+			this.lblFlags.Text = "99";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.ClientSize = new System.Drawing.Size(596, 526);
+			this.Controls.Add(this.lblFlags);
+			this.Controls.Add(this.lblTimer);
+			this.Controls.Add(this.btnStart);
+			this.Controls.Add(this.canvas);
+			this.Controls.Add(this.menuStrip1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MainMenuStrip = this.menuStrip1;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Minesweeper";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
--- Final_MineSweeper/Form1.cs
+++ Final_MineSweeper/Form1.cs
@@ -10,8 +10,8 @@ using System.Windows.Forms;
 
 namespace Final_MineSweeper
 {
-    public partial class Form1 : Form
-    {
+	public partial class Form1 : Form
+	{
 		public delegate void tileCallback(Tile t);
 		public Form1()
 		{
@@ -24,23 +24,27 @@ namespace Final_MineSweeper
 			mineCount = 10,
 			flagsRemaining = 99;
 		bool running = false;
+		DateTime startTime = DateTime.MinValue;
 		
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            NewMinefield();
-        }
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			NewMinefield();
+			timer1.Start();
+		}
 
-        public void NewGame()
+		public void NewGame()
 		{
 			NewMinefield();
 			GenerateMines();
-			CalculateDanger();
+			AddNeighbours();
 			flagsRemaining = mineCount;
+			canvas.Invalidate();
+			startTime = DateTime.MinValue;
 		}
 
-        public void NewMinefield()
-        {
+		public void NewMinefield()
+		{
 			mineField.Clear();
 			for (int i = 0; i < height; i++)
 			{
@@ -53,17 +57,17 @@ namespace Final_MineSweeper
 			}
 		}
 
-        private void Canvas_Paint(object sender, PaintEventArgs e)
-        {
+		private void Canvas_Paint(object sender, PaintEventArgs e)
+		{
 			mineField.ForEach((List<Tile> col) => {
 				col.ForEach((Tile t) => {
 					t.Draw(e.Graphics);
 				});
 			});
 
-        }
+		}
 
-        public void GenerateMines() {
+		public void GenerateMines() {
 			int bombsAdded = 0;
 			Random r = new Random();
 
@@ -78,39 +82,49 @@ namespace Final_MineSweeper
 			}
 		}
 
-        private void BtnStart_Click(object sender, EventArgs e)
-        {
+		private void BtnStart_Click(object sender, EventArgs e)
+		{
 			NewGame();
 			lblTimer.Text = "0";
-			timer1.Start();
 			running = true;
 		}
 
-		private void canvas_MouseClick(object sender, MouseEventArgs e)
+		private void Canvas_MouseClick(object sender, MouseEventArgs e)
 		{
 			if (!running) return;
 
-			//find block clicked
+			//find tile clicked
 			int x, y;
 			x = e.X/(Tile.size);
 			y = e.Y/(Tile.size);
+
+			// tell the tile it was clicked
 			mineField[x][y].Click(e);
+			
+			//calculate the number of flags remaining and display that to the user
 			flagsRemaining = CalculateRemainingFlags();
 			lblFlags.Text = flagsRemaining.ToString();
+			// re-draw the screen
 			canvas.Invalidate();
+			//if the timer hasn't been started then start it;
+			if (startTime.CompareTo(DateTime.MinValue) == 0) startTime = DateTime.Now;
 		}
 
 		private void Timer1_Tick(object sender, EventArgs e)
-        {
-           lblTimer.Text = (Convert.ToInt32(lblTimer.Text) + 1).ToString();
-        }
+		{
+			if (startTime.CompareTo(DateTime.MinValue) == 0) return;
+			if (!running) return;
 
-		public void CalculateDanger()
+			DateTime now = DateTime.Now;
+			TimeSpan time = now.Subtract(startTime);
+			lblTimer.Text = Math.Round(time.TotalSeconds).ToString();
+		}
+
+		public void AddNeighbours()
 		{
 			mineField.ForEach((List<Tile> col) => {
 				col.ForEach((Tile t) => {
-					t.bombsNear = 0;
-					ForNeighbors(t, t.AddBomb);
+					ForNeighbors(t, t.AddNeighbour);
 				});
 			});
 		}
@@ -209,6 +223,8 @@ namespace Final_MineSweeper
 			flagged
 		}
 
+		public List<Tile> neighbours = new List<Tile>();
+
 		public ClickedState state = ClickedState.unClicked;
 		public bool isBomb = false;
 		public byte bombsNear = 0;
@@ -222,27 +238,47 @@ namespace Final_MineSweeper
 			this.y = y;
 		}
 
-		public void AddBomb(Tile t)
+		public void CalculateDanger()
 		{
-			if (t.isBomb)
-			{
-				bombsNear++;
-			}
+			bombsNear = 0;
+			neighbours.ForEach((Tile t) => {
+				if (t.isBomb)
+					bombsNear++;
+			});
+		}
+
+		public void AddNeighbour(Tile t)
+		{
+			neighbours.Add(t);
+			CalculateDanger();
 		}
 
 		public void Click(MouseEventArgs e)
 		{
 			if (state == ClickedState.unClicked && e.Button == MouseButtons.Left)
 			{
-				state = Tile.ClickedState.clicked;
+				state = ClickedState.clicked;
+				if (bombsNear == 0)
+				{
+					neighbours.ForEach((Tile t) => t.Click(new MouseEventArgs(MouseButtons.Left, 1, 0, 0, 0)));
+				}
 			}
 			else if (state == ClickedState.unClicked && e.Button == MouseButtons.Right)
 			{
-				state = Tile.ClickedState.flagged;
+				state = ClickedState.flagged;
 			}
 			else if (state == ClickedState.flagged && e.Button == MouseButtons.Right)
 			{
-				state = Tile.ClickedState.unClicked;
+				state = ClickedState.unClicked;
+			}
+			else if (state == ClickedState.clicked && e.Button == MouseButtons.Right)
+			{
+				int flagsNear = 0;
+				neighbours.ForEach((Tile t) => { if (t.state == ClickedState.flagged) flagsNear++; });
+				if (flagsNear == bombsNear)
+				{
+					neighbours.ForEach((Tile t) => t.Click(new MouseEventArgs(MouseButtons.Left, 0, 0, 0, 0)));
+				}
 			}
 		}
 
@@ -251,49 +287,94 @@ namespace Final_MineSweeper
 			if (state.Equals(ClickedState.unClicked))
 			{
 				// fill
-				SolidBrush b = new SolidBrush(Color.Gray);
+				SolidBrush b = new SolidBrush(Color.FromArgb(192, 192, 192));
 				g.FillRectangle(b, x * size, y * size, size, size);
-				
+
 				// Border
-				Pen p = new Pen(Color.DarkGray, 2);
-				g.DrawLine(p, x * size + size - 1, y * size, x * size + size - 1, y * size + size);
-				g.DrawLine(p, x * size, y * size + size - 1, x * size + size, y * size + size - 1);
-				p.Color = Color.LightGray;
-				g.DrawLine(p, x * size, y * size, x * size, y * size + size);
-				g.DrawLine(p, x * size, y * size, x * size + size, y * size);
+				Pen p = new Pen(Color.Black, 2);
+
+				p.Color = Color.FromArgb(126, 126, 126);
+				g.DrawLine(p, x * size + size - 1, y * size, x * size + size - 1, y * size + size); // draw right edge
+				g.DrawLine(p, x * size, y * size + size - 1, x * size + size, y * size + size - 1); // draw bottom edge
+
+				p.Color = Color.White;
+				g.DrawLine(p, x * size + 1, y * size, x * size + 1, y * size + size); // draw left edge
+				g.DrawLine(p, x * size, y * size + 1, x * size + size, y * size + 1); // draw top edge
 
 			}
 			else if (state.Equals(ClickedState.clicked))
 			{
 				// fill
-				SolidBrush b = new SolidBrush(Color.Gray);
+				SolidBrush b = new SolidBrush(Color.FromArgb(192, 192, 192));
 				g.FillRectangle(b, x * size, y * size, size, size);
 
+				// Border
+				Pen p = new Pen(Color.FromArgb(133, 133, 133), 2);
 
-				g.DrawString(bombsNear.ToString(), new Font("Arial", 10), b, x * size, y * size);
+				g.DrawLine(p, x * size + 1, y * size, x * size + 1, y * size + size); // draw left edge
+				g.DrawLine(p, x * size, y * size + 1, x * size + size, y * size + 1); // draw top edge
 
+				if (isBomb)
+				{
+					// Draw Bomb
+					b.Color = Color.Black;
+					g.FillEllipse(b, x * size + 5, y * size + 5, size - 10, size - 10);
+				}
+				else
+				{
+					switch (bombsNear) // make text correct color
+					{
+						case 1:
+							b.Color = Color.Blue;
+							break;
+						case 2:
+							b.Color = Color.Green;
+							break;
+						case 3:
+							b.Color = Color.Red;
+							break;
+						case 4:
+							b.Color = Color.Navy;
+							break;
+						case 5:
+							b.Color = Color.FromArgb(128, 0, 0);
+							break;
+						case 6:
+							b.Color = Color.FromArgb(0, 128, 128);
+							break;
+						case 7:
+							b.Color = Color.Black;
+							break;
+						case 8:
+							b.Color = Color.FromArgb(128, 128, 128);
+							break;
+						default:
+							break;
+					}
+
+					g.DrawString(bombsNear.ToString(), new Font("Arial", 18), b, new RectangleF(x * size, y * size, size - 1, size - 1));
+				}
 			}
 			else if (state.Equals(ClickedState.flagged))
 			{
 				// fill
-				SolidBrush b = new SolidBrush(Color.Gray);
+				SolidBrush b = new SolidBrush(Color.FromArgb(192, 192, 192));
 				g.FillRectangle(b, x * size, y * size, size, size);
 
 				// Border
-				Pen p = new Pen(Color.DarkGray, 2);
-				g.DrawLine(p, x * size + size - 1, y * size, x * size + size - 1, y * size + size);
-				g.DrawLine(p, x * size, y * size + size - 1, x * size + size, y * size + size - 1);
-				p.Color = Color.LightGray;
-				g.DrawLine(p, x * size, y * size, x * size, y * size + size);
-				g.DrawLine(p, x * size, y * size, x * size + size, y * size);
+				Pen p = new Pen(Color.Black, 2);
 
-				g.DrawString(bombsNear.ToString(), new Font("Arial", 10), b, x * size, y * size);
+				p.Color = Color.FromArgb(126, 126, 126);
+				g.DrawLine(p, x * size + size - 1, y * size, x * size + size - 1, y * size + size); // draw right edge
+				g.DrawLine(p, x * size, y * size + size - 1, x * size + size, y * size + size - 1); // draw bottom edge
+
+				p.Color = Color.White;
+				g.DrawLine(p, x * size + 1, y * size, x * size + 1, y * size + size); // draw left edge
+				g.DrawLine(p, x * size, y * size + 1, x * size + size, y * size + 1); // draw top edge
 
 				// Flags
 				b.Color = Color.Red;
-				g.FillEllipse(b, x * size + 5 , y * size + 5, size - 10 , size - 10);
-
-				// On Right Click
+				g.FillEllipse(b, x * size + 5, y * size + 5, size - 10, size - 10);
 
 			}
 		}	
